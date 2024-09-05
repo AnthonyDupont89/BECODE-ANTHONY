64c7f23227754f2b9b88dc8f2dd438e68b3c411d
Anthony Dupont <anthony.dupont89@gmail.com>
2024-09-05 17:16:14 +0200

    06.Strings

---
+++ WEEK-01b-02/app.js
@@ -0,0 +1,11 @@
+//#15 String Method
+
+let x = "I am watching Dev Dreamer.";
+console.log(x.toUpperCase);
+
+console.log(x.indexOf("D")); //return 14
+console.log(x.indexOf(".")); //return 25
+
+console.log(x.slice(14, 25)); //return Dev Dreamer
+
+
