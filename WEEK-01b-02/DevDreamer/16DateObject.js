e07dc5ff7ce2c2c6e79a05fe047cd642540146b3
Anthony Dupont <anthony.dupont89@gmail.com>
2024-09-18 22:17:59 +0200

    28.Final

--- WEEK-01b-02/DevDreamer/16DateObject.js
+++ WEEK-01b-02/DevDreamer/16DateObject.js
@@ -7,3 +7,16 @@ console.log(`${hour}:${min}`);
 
 let day = date.toLocaleString('default', {weekday: 'long'})
 console.log(day);
+
+
+
+// !! l'objet est un reference type
+let date1 = new Date(2024, 8, 18);  // 18 septembre 2024
+let date2 = date1;  // Copie la référence de l'objet Date
+
+// Modifier la date via `date2`
+date2.setFullYear(2025);  // Change l'année à 2025
+
+console.log(date1);  // Affiche 18 septembre 2025 (modifié via `date2`)
+console.log(date2);  // Affiche 18 septembre 2025 (car les deux variables partagent la même référence)
+
--- WEEK-01b-02/DevDreamer/24TernaryOperator.js
+++ WEEK-01b-02/DevDreamer/24TernaryOperator.js
@@ -1,6 +1,41 @@
-let message = 1 > 10 ? "Condition is TRUE" : "Condition is FALSE";
+let age = 18;
+
+if (age >= 18) {
+    console.log("You can vote");
+} else {
+    console.log("Sorry, you can't vote");
+}
+
+let message0 = age >= 18 ? "You can vote" : "Sorry, you can't vote";
+console.log(message0);
+
+
+let password = "redruby123";
+let message1 = (password === "redruby123") ? 'Success' : 'Fail'; // Parentheses are not necessary; they just improve readability
+console.log(message1);
+
+
+
+let body = document.querySelector('body');
+let choice = '';
+choice === 'dark' ? body.classList.add('darkMode') : null; 
+
+
+
+let message = '';
+
+if (1 > 10) {
+    message = 'Condition is TRUE';
+} else {
+    message = 'Condition is FALSE';
+}
 console.log(message);
 
-let time = '12:00';
-let greeting = time < '12:00' ? "good morning" : "good evening";
+
+message = 1 > 10 ? 'Condition is TRUE' : 'Condition is FALSE';
+console.log(message);
+
+
+let time = '11:00';
+let greeting = time < '12:00' ? 'Good Morning' : 'Good Evening'; // !! Comparaison lexicographique (valeurs Unicode)
 console.log(greeting);
